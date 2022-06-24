4e93e2d0b4a3ebd54a2d9870d583e84f65d5681f
Ukasz3 <l.g.benhase@gmail.com>
2022-06-24 08:02:05 -0400

    add computers input function
    create function that gives back rock, paper, or scissors

--- function.js
+++ function.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+
+function computerPlay () {
+    let string = Math.floor(Math.random()*3);
+    if (string == 0) return "rock";
+    else if (string == 1) return "paper";
+    else if (string == 2) return "scissors";
+}
+console.log(computerPlay());
